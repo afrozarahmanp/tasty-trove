b7c67cd1e53f7c75e81f6d297137571cb20ab58d
Afroza Rahman <108660416+afrozarahmanp@users.noreply.github.com>
2024-01-06 14:54:25 +0600

    Featured items and testimonials added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import './Featured.css'
 
 const Featured = () => {
-    return (
+  return (
+    <div className="featured-item bg-fixed text-white pt-8 my-20 ">
+      <SectionTitle
+        heading="Featured Items"
+        subHeading="Check It Out"
+      ></SectionTitle>
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36 bg-black bg-opacity-40">
         <div>
-            
+          <img src={featuredImg} alt="" />
         </div>
-    );
+        <div className="md:ml-10">
+          <p>January 01, 2024</p>
+          <p className="uppercase">Where can i get some?</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            neque, corporis ipsa ea, error quas itaque nostrum ipsum quibusdam
+            illum ad praesentium ratione temporibus facere voluptas maiores,
+            iusto id fugiat. Obcaecati eum optio ut quos perspiciatis harum
+            consequatur, ad atque!
+          </p>
+          <button className="btn btn-outline text-white border-0 border-b-4">Order Now</button>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
