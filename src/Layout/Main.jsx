ca2c79e4a150a4ad5e67465b3bd72c9e286a156c
Afroza Rahman <108660416+afrozarahmanp@users.noreply.github.com>
2024-01-09 12:26:48 +0600

    login page created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import {Outlet} from "react-router-dom"
-import Footer from '../Pages/Shared/Footer/Footer';
-import NavBar from '../Pages/Shared/NavBar/NavBar';
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const Main = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const location = useLocation();
+  console.log(location);
+  const noHeaderFooter = location.pathname.includes("login");
+  return (
+    <div>
+      {noHeaderFooter || <NavBar></NavBar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || <Footer></Footer>}
+    </div>
+  );
 };
 
 export default Main;
